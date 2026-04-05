08ee011ff0b2866bf8b65c65643133c69b110678
Michael Hacker <mh@superbox.one>
2026-04-05 07:56:06 +0200

    Add tests

---
+++ tests/test_init.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from homeassistant.config_entries import ConfigEntryState
+
+from tests import setup_integration
+
+if TYPE_CHECKING:
+    from homeassistant.core import HomeAssistant
+    from pytest_homeassistant_custom_component.common import MockConfigEntry
+    from syrupy.assertion import SnapshotAssertion
+    from tests.conftest import FakeNetzOOEeServiceAPI
+
+
+async def test_load_entry(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    fake_api: FakeNetzOOEeServiceAPI,
+    snapshot: SnapshotAssertion,
+) -> None:
+    fake_api.register_auth_request()
+    fake_api.register_requests()
+
+    await setup_integration(hass, config_entry)
+
+    assert config_entry.state is ConfigEntryState.LOADED
+    assert hass.states.async_entity_ids_count() == 30
+
+    assert set(hass.states.async_entity_ids()) == snapshot
